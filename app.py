4930c388e514d5fd7eda1ee636c272aa29b35d26
kat-does-biotech <kknip1643@gmail.com>
2026-08-07 14:09:15 -0500

    infographic debug 2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from lib.data_loader import load_data
-from lib.ui import render_breadcrumbs, render_categories, render_list, render_detail, render_network_map
+from lib.ui import render_breadcrumbs, render_categories, render_list, render_detail, render_network_map, render_infographic
 
 st.set_page_config(page_title="Houston Biotech Ecosystem Map", layout="wide")
 
@@ -43,20 +43,14 @@ if st.session_state.node:
 elif "node" in st.query_params:
     del st.query_params["node"]
 
-st.title("Houston biotech ecosystem map")
+st.title("Houston Biotech Ecosystem Map")
 st.caption("Developed and maintained by [Kaitlyn Sanchez-Nussberger](https://www.linkedin.com/in/kaitlyn-sanchez-nussberger/)")
 st.caption("An inexhaustive list")
 
-if st.session_state.screen == "categories":
-    render_infographic(institutions, edges, stats)
-    render_network_map(institutions, edges, rel_types, go_node)
-    st.markdown("#### Browse by category")
-    render_categories(institutions, go_category)
-
 render_breadcrumbs(institutions, go_categories, go_category)
 
-
 if st.session_state.screen == "categories":
+    render_infographic(institutions, edges, stats)
     render_network_map(institutions, edges, rel_types, go_node)
     st.markdown("#### Browse by category")
     render_categories(institutions, go_category)
